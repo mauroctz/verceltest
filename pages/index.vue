cd418f2803a9f6985757d7f99a7024753259af20
mauro <mauro@ubu-team.com>
2020-10-16 10:12:01 +0200

    change text

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div>
       <logo />
       <h1 class="title">
-        nuxtjs
+        PRIMO TEST
       </h1>
       <h2 class="subtitle">
-        PRIMO TEST
+        mauro cotzia
       </h2>
       <div class="links">
         <a
